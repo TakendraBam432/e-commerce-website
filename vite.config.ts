312f31a562adddf0b93f23ba4de8799ad2db8a31
Takendra Bam <takendrabam051@gmail.com>
2025-11-02 23:21:53 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // should be '/' for app.takendrabam.com.np
+  base: "./", // should be '/' for app.takendrabam.com.np
   server: {
     host: "::",
     port: 8080,
